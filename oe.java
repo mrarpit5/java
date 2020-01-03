6640e80bc88a93863e9e147082246a51c56a19fa
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-03 22:56:46 +0530

    check number is odd or even

---
+++ oe.java
@@ -0,0 +1,16 @@
+class oe
+{
+	public static void main(String[] args) 
+	{
+		int n;
+		n = Integer.parseInt(args[0]);
+		if(n%2==0)
+		{
+			System.out.println("even");
+		}	
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+}
