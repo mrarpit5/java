34acd11c80bb7569c6e3bf8cb60662714c193537
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-03 22:38:21 +0530

    big from 2 numbers

---
+++ bigfrom2.java
@@ -0,0 +1,17 @@
+class bigfrom2
+{
+	public static void main(String[] args) 
+	{
+		int a,b;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		if(a>b)
+		{
+			System.out.println("a is big");
+		}
+		else
+		{
+			System.out.println("b is big");
+		}	
+	}
+}
