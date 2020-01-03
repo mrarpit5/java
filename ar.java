b16b71f0b92d115c0234e1dcf5c207d30663777c
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-03 22:33:10 +0530

    Scanner class

---
+++ ar.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ar
+{
+	public static void main(String[] args) {
+	Scanner sc = new Scanner();
+	int[] arr = new int[10];
+	for(int i=0;i<=10;i++)
+	{
+		System.out.println("");
+	
+	}
+} 
+
+}
