16682bdcab3b9af9c0c8789c32121b4175a89e85
arpit khandhar <59481203+mrarpit5@users.noreply.github.com>
2020-01-03 23:11:47 +0530

    table of a particular number

---
+++ table.java
@@ -0,0 +1,14 @@
+class table
+{
+	public static void main(String[] args) 
+	{
+		int i,n,r;
+		n = Integer.parseInt(args[0]);
+		for(i=1;i<11;i++)
+		{
+			r=n*i;
+			System.out.println(n +" * "+ i +" = "+ r);
+			
+		}	
+	}
+}
